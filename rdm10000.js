7b050a08c38ee5d09ca84b6dea5a89c9bd3d30b3
Alvin <alvin.tol@hotmail.com>
2022-06-27 18:15:30 -0600

    adds duplicate number check in list per each loop

--- rdm10000.js
+++ rdm10000.js
@@ -3,7 +3,7 @@
 // Initialize empty state of number list
 // Create loop that generates numbers  
 // Create random number call
-
+// Create duplicate list number check
 
 // immediately invoked function expression
 (() => {
@@ -12,13 +12,25 @@
   const list = [];
 
   // Variable number length for easier length change
-  const maxNum = 10;
+  const maxNum = 10000;
+
+  // Random number generator
   const randomNum = () => Math.ceil(Math.random() * maxNum)
 
   console.log('start', `\n`, list)
 
+  // Continuous loop that persists while the list of integers is less than desired length
   while (list.length < maxNum) {
-    list.push(randomNum())
+
+    // Locally generated number for each loop instance
+    const newNum = randomNum();
+    
+    // Duplicate integer check in current state of list[]
+    if (!list.find(num => num === newNum)) {
+
+    // if krandomly generated number is unique. Add to list
+      list.push(newNum)
+    }
   }
 
   console.log(...list, `\n`, 'end' )
